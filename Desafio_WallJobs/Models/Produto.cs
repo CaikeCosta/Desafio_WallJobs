54845c651e4754c36d480c3f36fc2ce4252d901a
Caike Costa <caikecostalima123@gmail.com>
2024-01-22 15:33:48 -0300

    Adicionar arquivos de projeto.

---
+++ Desafio_WallJobs/Models/Produto.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Desafio_WallJobs.Models
+{
+
+    [Table("Produto")]
+    public class Produto
+    {
+        [Column("Id")]
+        [Display(Name = "Codigo")]
+        public int Id { get; set; }
+
+        [Column("Nome")]
+        [Display(Name = "Nome")]
+
+        public string Nome { get; set; }
+
+        [Column("Descricao")]
+        [Display(Name ="Descricao")]
+        public string Descricao { get; set; }
+
+        [Column("Preco")]
+        [Display(Name = "Preco")]
+        public string Preco { get; set; }
+    }
+}
